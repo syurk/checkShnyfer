7baa0d4d1a550dac9cb188cfc847911ccff8449c
hsteg492 <hsteg492@students.bju.edu>
2018-09-13 10:44:22 -0400

    added addchecks.html and updated views.py

--- src/addchecks/views.py
+++ src/addchecks/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world")
+    with open('addchecks.html', 'r') as myfile:
+        data = myfile.read()
+    return HttpResponse(data)
