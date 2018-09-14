a03f769b7d68c6229acef3bf597964345efdb9a3
bstee615 <bstee615@students.bju.edu>
2018-09-13 21:24:15 -0400

    Added addchecks/viewchecks page.

--- src/addchecks/views.py
+++ src/addchecks/views.py
@@ -7,14 +7,15 @@ def index(request):
     return HttpResponse("Hello, world.")
 
 def viewchecks(request):
-    return HttpResponse(', '.join(map(lambda c: checkToString(c),Check.objects.order_by('-written_date')[:5])))
+    return HttpResponse(', '.join(map(lambda c: checkToString(c),Check.objects.order_by('-written_date'))))
 
 def checkToString(check):
     return """
-    <p>Name: {}</p>
-    <p>Date Written: {}</p>
-    <p>Amount: {}</p>
+    <h3>Name: {}</h3>
+    <h4>Date Written: {}</h4>
+    <h4>Amount: {}</h4>
     <p>Routing Number: {}</p>
     <p>Account Number: {}</p>
     <p>Check Number: {}</p>
-    """.format(c.name, c.written_date, c.amount, c.routing_no, c.account_no, c.check_no)
+    </hr>
+    """.format(check.name, check.written_date, check.amount, check.routing_no, check.account_no, check.check_no)
