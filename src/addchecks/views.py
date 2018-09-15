4132852e488c17b331ccc44e1519883da8e57663
bstee615 <bstee615@students.bju.edu>
2018-09-14 22:46:14 -0400

    Added links from addchecks to viewchecks.

--- src/addchecks/views.py
+++ src/addchecks/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.utils import timezone
 
-import datetime
+from datetime import datetime
 
 from .models import Check
 
@@ -14,25 +14,14 @@ def submit(request):
     return HttpResponse("Submitted check. <a href='../viewchecks'>Click here to view all checks.</a>")
 
 def viewchecks(request):
-    return HttpResponse(', '.join(map(lambda c: checkToString(c),Check.objects.order_by('-written_date'))))
+    return render(request, "addchecks/viewchecks.html", {"checks": Check.objects.order_by('-written_date').exclude(name="")})
 
 def addCheckToDB(request):
     c = Check()
     c.name = request.POST.get("fullname")
-    c.written_date = request.POST.get("checkdate")
+    c.written_date = timezone.make_aware(datetime.strptime(request.POST.get("checkdate"), "%Y-%m-%d"))
     c.amount = request.POST.get("dollaramount")
     c.routing_no = request.POST.get("routingnumber")
     c.account_no = request.POST.get("accountnumber")
     c.check_no = request.POST.get("checknumber")
     c.save()
-
-def checkToString(check):
-    return """
-    <h3>Name: {}</h3>
-    <h4>Date Written: {}</h4>
-    <h4>Amount: {}</h4>
-    <p>Routing Number: {}</p>
-    <p>Account Number: {}</p>
-    <p>Check Number: {}</p>
-    </hr>
-    """.format(check.name, check.written_date, check.amount, check.routing_no, check.account_no, check.check_no)
