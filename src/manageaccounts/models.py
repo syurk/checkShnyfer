ee6badd0d91f5a75d0d41cb3d91407dcfd8e3764
hsteg492 <hsteg492@students.bju.edu>
2018-10-27 19:57:54 -0400

    added input validation and fixed phone_number issue

--- src/manageaccounts/models.py
+++ src/manageaccounts/models.py
@@ -5,8 +5,7 @@ from django.core.validators import RegexValidator
 class Account(models.Model):
     account_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=150)
-    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
-    phone_number = models.CharField(validators=[phone_regex], max_length=17) # validators should be a list
+    phone_number = models.CharField(max_length=12)
     routing_no = models.PositiveIntegerField('routing number')
     account_no = models.PositiveIntegerField('account number')
     street_address = models.CharField(max_length=150)
