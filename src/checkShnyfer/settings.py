75d83f80da25834859cc00f2ebe6e61a697c5b35
hsteg492 <hsteg492@students.bju.edu>
2018-11-29 10:12:47 -0500

    added templates directory

--- src/checkShnyfer/settings.py
+++ src/checkShnyfer/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'checkShnyfer.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
